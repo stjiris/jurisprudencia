29031badedec9d4db57efbcaa4ffa5e9d28e96ed
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2022-03-31 09:34:38 +0100

    Replace Tribunal Codes

--- cms-indexer.js
+++ cms-indexer.js
@@ -37,6 +37,7 @@ async function forEachCSMRecord(fn){
 
 console.log("Starting...");
 forEachCSMRecord(async record => {
+    record.ecli = record.ecli.replace(":TCONS:", ":TCO:").replace(":TCONF:", ":COF:")
     let ECLI = ECLI_Builder.fromString(record.ecli);
     if( await fs.readFile(`./data/${record.ecli}.html`).then(_ => true).catch(_ => false) ){ return false; }
     if( await findEcli(ECLI) ){ return true; }
