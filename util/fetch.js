8fc2c85f84944b838904cfa6339da9cc1a5573c2
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2022-04-09 05:06:21 +0100

    utils

---
+++ util/fetch.js
@@ -0,0 +1,49 @@
+const https = require('https');
+const http = require('http');
+const { URL } = require("url");
+const { JSDOM } = require("jsdom");
+
+const sleep = (time) => new Promise(resolve => setTimeout(resolve, time))
+
+const fetch = module.exports = (url, options={}) => new Promise((resolve, reject) => {
+    let urlObj = new URL(url);
+    let resFunction = (res) => {
+        let data = "";
+        res.on('data', (d) => data += d);
+        res.on('end', () => {
+            resolve(data);
+        });
+    }
+    if( urlObj.protocol == "https:" ){
+        https.get(url, options, resFunction).on('error', reject);
+    } else {
+        http.get(url, options, resFunction).on('error', reject);
+    }
+});
+
+const fetchRetry = module.exports.fetchRetry = async (url, options={}) => {
+    let result = null;
+    let interval = 1;
+    while( result == null ){
+        result = await fetch(url, options).catch(async e => {
+            console.log(`fetchRetry(${url}) in ${interval}s: ${e.message}`);
+            await sleep(interval*1000);
+            interval *= 2;
+            return null;
+        });
+    }
+    return result;
+}
+
+const DOM_DEFAULT_OPTIONS = {
+    headers: {
+        'Accept': 'text/html,application/xhtml+xml,application/xml',
+        'Accept-Language': 'pt-PT,pt',
+        'Accept-Charset': 'utf-8'
+    }
+}
+
+module.exports.json = (url, options={}) => fetchRetry(url, options).then(JSON.parse);
+module.exports.dom = (url, options=DOM_DEFAULT_OPTIONS) => fetchRetry(url, options).then(dom => new JSDOM(dom, { url: url }));
+
+
