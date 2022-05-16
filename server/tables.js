4829ef50be922c4f39c8c2026a306c59f6da7b68
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2022-05-16 12:33:55 +0100

    Fix link of sse & new table

--- server/tables.js
+++ server/tables.js
@@ -40,7 +40,32 @@ defineTable("ano-origem", ()=>agg({
     let header = ["Ano\\Origem", "Total", ...origens];
     let values = aggs.Ano.buckets.map(b => [b.key_as_string, b.doc_count, ...(b.Origem.buckets.length > 0 ? b.Origem.buckets.sort((b1,b2) => b1.key.localeCompare(b2.key)) : origens.map(() => ({doc_count:0}))).map(b => b.doc_count)]);
     return {header, values};
-}))
+}));
+
+defineTable("ano-tribunal", ()=>agg({
+    Ano: {
+        date_histogram: {
+            field: "Data",
+            interval: "year",
+            min_doc_count: 0,
+            format: "yyyy"
+        },
+        aggs: {
+            Tribunal: {
+                terms: {
+                    field: "Código Tribunal",
+                    size: 15,
+                    min_doc_count: 0
+                }
+            }
+        }
+    }
+}).then(aggs => {
+    let tribunais = aggs.Ano.buckets[0].Tribunal.buckets.map(bucket => bucket.key).sort((b1,b2) => b1.localeCompare(b2));
+    let header = ["Ano\\Tribunal", "Total", ...tribunais];
+    let values = aggs.Ano.buckets.map(b => [b.key_as_string, b.doc_count, ...(b.Tribunal.buckets.length > 0 ? b.Tribunal.buckets.sort((b1,b2) => b1.key.localeCompare(b2.key)) : tribunais.map(() => ({doc_count:0}))).map(b => b.doc_count)]);
+    return {header, values};
+}));
 
 defineTable("ecli-errado", () => indexer._client.search({
     index: indexer.mapping.index,
