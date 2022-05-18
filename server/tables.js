06e7ea0caafb09c11a15d10d63cb547124557cb2
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2022-05-18 02:45:28 +0100

    Partition repeated ECLI

--- server/tables.js
+++ server/tables.js
@@ -14,7 +14,7 @@ const agg = (obj) => indexer._client.search({
 const tables = [];
 function defineTable(name, cb){
     tables.push(name);
-    app.get(`/${name}`, (req, res) => cb().then( ({header, values}) => res.render("tables", {header, values}) ).catch(e => res.render("tables", {header: [], values: [], error: e})||console.log(e)));
+    app.get(`/${name}`, (req, res) => cb(req).then( (obj) => res.render("tables", {...obj}) ).catch(e => res.render("tables", {header: [], values: [], error: e})||console.log(e)));
 }
 
 defineTable("ano-origem", ()=>agg({
@@ -88,5 +88,44 @@ defineTable("ecli-errado", () => indexer._client.search({
     const values = res.hits.hits.map((hit) => [hit._source.ECLI, hit._source._UNMATCHING_ECLI]);
     return {header, values};
 }));
+
+const PARTITIONS = Math.ceil(317685 / 10000);
+
+defineTable("ecli-repetido", (req)=>agg({
+    ECLI: {
+        terms: {
+            field: "ECLI",
+            include: {
+                partition: req.query.p || 0,
+                num_partitions: PARTITIONS
+            },
+            size: 317685,
+            min_doc_count: 2
+        },
+        aggs: {
+            Origem: {
+                terms: {
+                    field: "Origem",
+                    size: 10
+                }
+            }
+        }
+    }
+}).then(aggs => {
+    let header = ["ECLI", "Total", "Origem"];
+    let values = aggs.ECLI.buckets.map(b => [`<a href="../${b.key}">${b.key}</a>`, b.doc_count, b.Origem.buckets.map(b => b.key).join(", ")]);
+    if( aggs.ECLI.sum_other_doc_count > 0 ){
+        values.unshift([`<b>Outros ECLI não listados:</b> ${aggs.ECLI.sum_other_doc_count}</b>`, "", ""]);
+    }
+    let p = parseInt(req.query.p || "0");
+    return {header, values, warning: `
+        <p>A ver partição ${p+1} de ${PARTITIONS}.</p>
+        <ul>Ir para:
+            <li><a href="?p=0">Primeira partição</a></li>
+            ${ p > 0  ? `<li><a href="?p=${p-1}">Partição anterior</a></li>` : "" }
+            ${ p < 60  ? `<li><a href="?p=${p+1}">Partição Seguinte</a></li>` : "" }
+            <li><a href="?p=${PARTITIONS-1}">Última partição</a></li>
+        </ul>`};
+}))
 
 app.get("/", (req, res) => res.render("tables", {header: ["Tabelas"], values: tables.map((t) => [`<a href="./${t}">${t}</a>`])}));
