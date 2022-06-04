6f6f9ecc285ad2d45a5612d55a925037bf6d1af4
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2022-06-04 02:03:37 +0100

    Acordaos com so um descritor não tinham n

--- fix_descritores.js
+++ fix_descritores.js
@@ -4,8 +4,19 @@ let client = indexer._client;
 client.search({
     index: indexer.mapping.index,
     query: {
-        wildcard: {
-            "Descritores.keyword": "*\n*"
+        bool: {
+            must: [
+                {
+                    wildcard: {
+                        "Descritores.keyword": "*\n*"
+                    }
+                },
+                {
+                    term: {
+                        "Origem": "csm-indexer"
+                    }
+                }
+            ]
         }
     },
     scroll: '30s',
@@ -26,4 +37,53 @@ client.search({
             scroll: '30s'
         });
     }
-})
+
+    res = await client.search({
+        index: indexer.mapping.index,
+        query: {
+            bool: {
+                must_not: [
+                    {
+                        wildcard: {
+                            "Descritores.keyword": "*\n*"
+                        }
+                    },
+                    {
+                        wildcard: {
+                            "Descritores.keyword": "* *"
+                        }
+                    }
+                ],
+                must: [
+                    {
+                        exists: {
+                            field: "Descritores"
+                        }
+                    },
+                    {
+                        term: {
+                            "Origem": "csm-indexer"
+                        }
+                    }
+                ]
+            }
+        },
+        scroll: '30s',
+        _source: ["Descritores"]
+    });
+    while(res.hits.hits.length){
+        for(let hit of res.hits.hits){
+            await client.update({
+                id: hit._id,
+                index: indexer.mapping.index,
+                doc: {
+                    Descritores: [hit._source.Descritores.join(" ")]
+                }
+            })
+        }
+        res = await client.scroll({
+            scroll_id: res._scroll_id,
+            scroll: '30s'
+        });
+    }
+});
