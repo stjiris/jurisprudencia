95916235df2b05fef9601e31ed171051f075afe8
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2022-06-21 11:34:44 +0100

    init

--- dgsi-indexer-stj-only.js
+++ dgsi-indexer-stj-only.js
@@ -1,9 +1,13 @@
 const fetch = require('./util/fetch.js');
 const { Worker } = require("worker_threads")
 const { init } = require("./indexer")
-            
-new Worker("./dgsi-indexer-worker.js", {workerData: {
-    Tribunal: "Supremo Tribunal de Justiça",
-    TribunalCode: "STJ",
-    link : "http://www.dgsi.pt/jstj.nsf?OpenDatabase"
-}})
+
+init().then( async _ => {
+    new Worker("./dgsi-indexer-worker.js", {workerData: {
+        Tribunal: "Supremo Tribunal de Justiça",
+        TribunalCode: "STJ",
+        link : "http://www.dgsi.pt/jstj.nsf?OpenDatabase"
+    }})
+}).catch(e => {
+    console.log(e)
+});
