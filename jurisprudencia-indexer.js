2f9f1fffd95ff680eea941590e15f5c420f0d95e
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2022-08-18 03:02:29 +0100

    New version

---
+++ jurisprudencia-indexer.js
@@ -0,0 +1,123 @@
+const url2table = require('./url-to-table');
+const es = require('@elastic/elasticsearch')
+const client = new es.Client({ node: 'http://localhost:9200' });
+const DGSI_LINK = "http://www.dgsi.pt/jstj.nsf?OpenDatabase";
+const DGSI_PATTERN = /http:\/\/www\.dgsi\.pt\/jstj\.nsf\/(?<hashsjt>.*)\/(?<hashid>.*)\?OpenDocument/;
+const jurisprudencia = require('./jurisprudencia');
+const fetch = require('./util/fetch');
+const { strip_attrs } = require('./util/html');
+
+forEachDgsiLink(async url => {
+    if( await UrlIsIndexed(url) ){
+        console.log(`${url} already indexed`)
+        return;
+    }
+    let table = await url2table(url);
+    let original = {}
+    let tipo = "Acordão";
+    let data = "01/01/1900";
+    Object.keys( table ).forEach( key => {
+        if( key.startsWith("Data") ){
+            original[key] = table[key].textContent.trim().replace(/-/g, '/')
+        }
+        else{
+            original[key] = table[key].innerHTML;
+        }
+        if( key.match(/Data d. (.*)/) ){
+            tipo = key.match(/Data d. (.*)/)[1];
+            data = table[key].textContent.trim().replace(/-/g, '/');
+        }
+    });
+    let object = {
+        "Original": original,
+        "Tipo": tipo,
+        "Processo": table.Processo.textContent.trim().replace(/\s-\s.*$/, "").replace(/ver\s.*/, ""),
+        "Data": data,
+        "Relator": table["Relator"].textContent.trim(),
+        "Descritores": getDescritores(table),
+        "Meio Processual": getMeioProcessual(table),
+        "Votação": getVotação(table),
+        "Secção": getSeccao(table),
+        "Sumário": strip_attrs(table["Sumário"]?.innerHTML || ""),
+        "Texto": strip_attrs(table["Decisão Texto Integral"]?.innerHTML || ""),
+        "URL": url
+    }
+    await client.index({
+        index: jurisprudencia.Index,
+        body: object
+    });
+})
+
+function getDescritores(table){
+    if( table.Descritores ){
+        // TODO: handle , and ; in descritores (e.g. "Ação Civil; Ação Civil e Administrativa") however dont split some cases (e.g. "Art 321º, do código civil")
+        return table.Descritores.textContent.trim().split(/\n|;/).map( desc => desc.trim().replace(/\.$/g,'').replace(/^(:|-|,|"|“|”|«|»|‘|’)/,'').trim() ).filter( desc => desc.length > 0 )
+    }
+    return []
+}
+
+function getMeioProcessual(table){
+    if( table.MeioProcessual ){
+        return table.MeioProcessual.textContent.trim();
+    }
+    return "";
+}
+
+function getVotação(table){
+    if( table.Votação ){
+        let text = table.Votação.textContent.trim();
+        if( text.match(/unanimidade/i) ){
+            return {
+                "Forma": "Unanimidade"
+            };
+        }
+        else{
+            return {
+                "Forma": text,
+                "Voto Vencido": 100,
+                "Declaração de Voto": 100,
+                "Voto de Desempate": 100
+            };
+        }
+    }
+    return "";
+}
+
+function getSeccao(table){
+    if( table["Nº Convencional"] ){
+        return table["Nº Convencional"].textContent.trim();
+    }
+    return "";
+}
+
+async function UrlIsIndexed( url ){
+    let res = await client.search({
+        index : jurisprudencia.Index,
+        query: {
+            term: {
+                URL: url
+            }
+        }
+    })
+    return res.hits.total.value > 0;
+}
+
+async function forEachDgsiLink( fn ){
+    let visited = {}
+    let currurl = DGSI_LINK;
+    while( true ){
+        let page = await fetch.dom(currurl);
+        let anchorList = Array.from(page.window.document.querySelectorAll("a"))
+        let next = anchorList.find( l => l.textContent == "Seguinte" ).href
+        let courtList = anchorList.map(a => a.href).filter(u => u.match(DGSI_PATTERN))
+        for( let decision of courtList ){
+            if( decision in visited ) continue;
+            visited[decision] = true;
+            await fn(decision);
+        }
+        if( next == currurl ){
+            break;
+        }
+        currurl = next
+    }
+}
---
+++ jurisprudencia.js
@@ -0,0 +1,130 @@
+const es = require('@elastic/elasticsearch')
+const client = new es.Client({ node: 'http://localhost:9200' });
+
+const Index = module.exports.Index = "jurisprudencia.5.0";
+const Properties = module.exports.Properties = {
+    "Original": {
+        type: 'object',
+        enabled: false
+    },
+    "Tipo": {
+        type: 'keyword',
+        normalizer: 'term_normalizer'
+    },
+    "Processo": {
+        type: 'keyword',
+        normalizer: 'term_normalizer'
+    },
+    "Data": {
+        type: 'date',
+        format: 'dd/MM/yyyy'
+    },
+    "Relator": {
+        type: 'keyword',
+        normalizer: 'term_normalizer'
+    },
+    "Descritores": {
+        type: 'text',
+        fielddata: true,
+        fields: {
+            "keyword": {
+                type: 'keyword',
+                normalizer: 'term_normalizer'
+            }
+        }
+    },
+    "Meio Processual": {
+        type: 'text',
+        fielddata: true,
+        fields: {
+            "keyword": {
+                type: 'keyword',
+                normalizer: 'term_normalizer'
+            }
+        }
+    },
+    "Votação": {
+        type: 'object',
+        properties: {
+            "Forma": {
+                type: 'keyword',
+                normalizer: 'term_normalizer'
+            },
+            "Voto Vencido": {
+                type: 'float'
+            },
+            "Declaração de Voto": {
+                type: 'float'
+            },
+            "Voto de Desempate": {
+                type: 'float'
+            }
+        }
+    },
+    "Secção": {
+        type: 'keyword',
+        normalizer: 'term_normalizer'
+    },
+    "Sumário": {
+        type: 'text',
+        term_vector: 'with_positions_offsets_payloads'
+    },
+    "Texto": {
+        type: 'text',
+        term_vector: 'with_positions_offsets_payloads'
+    },
+    "URL": {
+        type: 'keyword',
+    }
+}
+
+module.exports.delete = () => client.indices.delete({ index: Index });
+module.exports.exists = () => client.indices.exists({ index: Index });
+module.exports.create = () => client.indices.create({
+    index: Index,
+    mappings: {
+        dynamic_date_formats: ['dd/MM/yyyy'],
+        properties: Properties
+    },
+    settings: {
+        analysis: {
+            normalizer: {
+                term_normalizer: {
+                    type: 'custom',
+                    filter: ['uppercase', 'asciifolding'],
+                }
+            },
+            analyzer: {
+                default: {
+                    char_filter: ['html_strip'],
+                    filter: ['trim', 'lowercase', 'asciifolding', "stopwords_pt"],
+                    tokenizer: 'classic',
+                }
+            },
+            filter: {
+                stopwords_pt: {
+                    type: 'stop',
+                    ignore_case: true,
+                    stopwords: ["de", "a", "o", "que", "e", "do", "da", "em", "um", "para", "com", "não", "uma", "os", "no", "se", "na", "por", "mais", "as", "dos", "como", "mas", "ao", "ele", "das", "à", "seu", "sua", "ou", "quando", "muito", "nos", "já", "eu", "também", "só", "pelo", "pela", "até", "isso", "ela", "entre", "depois", "sem", "mesmo", "aos", "seus", "quem", "nas", "me", "esse", "eles", "você", "essa", "num", "nem", "suas", "meu", "às", "minha", "numa", "pelos", "elas", "qual", "nós", "lhe", "deles", "essas", "esses", "pelas", "este", "dele", "tu", "te", "vocês", "vos", "lhes", "meus", "minhas", "teu", "tua", "teus", "tuas", "nosso", "nossa", "nossos", "nossas", "dela", "delas", "esta", "estes", "estas", "aquele", "aquela", "aqueles", "aquelas", "isto", "aquilo", "estou", "está", "estamos", "estão", "estive", "esteve", "estivemos", "estiveram", "estava", "estávamos", "estavam", "estivera", "estivéramos", "esteja", "estejamos", "estejam", "estivesse", "estivéssemos", "estivessem", "estiver", "estivermos", "estiverem", "hei", "há", "havemos", "hão", "houve", "houvemos", "houveram", "houvera", "houvéramos", "haja", "hajamos", "hajam", "houvesse", "houvéssemos", "houvessem", "houver", "houvermos", "houverem", "houverei", "houverá", "houveremos", "houverão", "houveria", "houveríamos", "houveriam", "sou", "somos", "são", "era", "éramos", "eram", "fui", "foi", "fomos", "foram", "fora", "fôramos", "seja", "sejamos", "sejam", "fosse", "fôssemos", "fossem", "for", "formos", "forem", "serei", "será", "seremos", "serão", "seria", "seríamos", "seriam", "tenho", "tem", "temos", "tém", "tinha", "tínhamos", "tinham", "tive", "teve", "tivemos", "tiveram", "tivera", "tivéramos", "tenha", "tenhamos", "tenham", "tivesse", "tivéssemos", "tivessem", "tiver", "tivermos", "tiverem", "terei", "terá", "teremos", "terão", "teria", "teríamos", "teriam"]
+                }
+            }
+        },
+        number_of_shards: 1,
+        number_of_replicas: 0,
+        max_result_window: 550000
+    }
+});
+
+if( require.main == module ){
+    module.exports.exists().then( async exists => {
+        if( !exists ){
+            console.log("Creating index...", Index);
+            await module.exports.create();
+        }
+        else{
+            console.log("Index already exists", Index);
+        }
+    }).catch( err => {
+        console.log( err );
+    });
+}
