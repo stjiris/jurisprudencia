4aa0454fd0e1d78b1bdf9778cd87d3e031485bb6
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2023-03-09 18:14:33 +0000

    Change name of field

--- jurisprudencia-indexer.js
+++ jurisprudencia-indexer.js
@@ -126,7 +126,7 @@ forEachDgsiLink(async url => {
         "Fonte": "STJ (DGSI)",
         "ECLI": "sem ECLI",
         "Jurisprudência": "Não",
-        "Revista excecional": "Não",
+        "Revista Excecional": "Não",
         "CONTENT": CONTENT,
         "URL": url
     }
