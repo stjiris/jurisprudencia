ce73e17cebb63e5b4db31ea52c4cdde6a3db7885
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2023-03-09 11:23:42 +0000

    Assign ecli script

---
+++ assign-ecli.js
@@ -0,0 +1,65 @@
+const es = require('@elastic/elasticsearch');
+const { appendFileSync } = require('fs');
+const client = new es.Client({ node: process.env.ES_URL || 'http://localhost:9200' });
+const jurisprudencia = require('./jurisprudencia');
+const ECLI = require("./util/ecli")
+
+let know_eclis = [];
+require("fs").readFileSync("eclis.txt").toString().split("\n").forEach( e => e != '' && know_eclis.indexOf(e) == -1 ? know_eclis.push(e) : null )
+
+function findMathingECLI(ecli){
+    return know_eclis.filter( o => o.indexOf(ecli) == 0)
+}
+
+let builder = new ECLI().setCountry("PT").setJurisdiction("STJ").setYear("0000");
+
+async function getOfficialECLI(process, year){
+    let maybeECLI = builder.setYear(year).setNumber(process).build();
+    let cached = findMathingECLI(maybeECLI);
+    if( cached.length > 1 ) return null; // Too many matching values
+    let trueECLI = cached[0];
+    if( !trueECLI ){
+        let url = `https://jurisprudencia.csm.org.pt/items/loadItems?queries[courts][]=1&sorts[dataAcordao]=-1&queries[filter_unique_number]=${encodeURIComponent(process)}`
+        console.error("Fetching", url)
+        let r = await fetch.json(url);
+        await fetch.sleep(10*1000);
+        if( r.records.length == 1 ){
+            trueECLI = r.records[0].ecli;
+        }
+        if( trueECLI && findMathingECLI(trueECLI).length == 0 ){
+            appendFileSync("eclis.txt", trueECLI+'\n')
+        }
+    }
+    return trueECLI;
+}
+
+const fetch = require('./util/fetch');
+
+client.search({
+    index: jurisprudencia.Index,
+    query: {
+        term: {
+            ECLI: "sem ECLI"
+        }
+    },
+    _source: ["Processo","Data"],
+    scroll: '3m'
+}).then(async r => {
+    while(r.hits.hits.length > 0){
+        for( let hit of r.hits.hits ){
+            let year = hit._source["Data"].match(/\d{4}/)[0];
+            let proc = hit._source["Processo"];
+            let ecli = await getOfficialECLI(proc, year);
+            if( ecli ){
+                await client.update({
+                    index: hit._index,
+                    id: hit._id,
+                    doc: {
+                        ECLI: ecli
+                    }
+                })
+            }
+        }
+        r = await client.scroll({scroll: '1m', scroll_id: r._scroll_id})
+    }
+})
