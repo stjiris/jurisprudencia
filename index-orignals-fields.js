dead4f426230436730b0db2674a6f39b81332b71
Diogo Almiro <13120173+diogoalmiro@users.noreply.github.com>
2023-04-19 11:00:07 +0100

    Testing option

--- index-orignals-fields.js
+++ index-orignals-fields.js
@@ -14,6 +14,7 @@ client.indices.create({index: `${Index}.original`, settings: {
             match_mapping_type: "string",
             mapping: {
                 type: "keyword",
+                index: "not_analyze"
             }
         }}
     ]
@@ -36,6 +37,7 @@ client.indices.create({index: `${Index}.original`, settings: {
                 if( key.match(/data/i) && hit._source.Original[key] == "N/D" || obj[key].length == 0 ) {
                     delete obj[key];
                 }
+                if( obj[key].length > )
             }
     	    await client.index({index: `${Index}.original`, document: obj, id: hit._id, version: hit._version})
         }
